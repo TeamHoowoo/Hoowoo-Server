b892354ca6be16ee456d512da5d37a7a002ebc08
ebulsok <leebulsok@gmail.com>
2022-12-02 21:06:54 +0900

    init

---
+++ app.js
@@ -0,0 +1,28 @@
+// @ts-check
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+const router = require('./routes/index');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+// app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(cors());
+
+app.use('/', router);
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(err.statusCode || 500);
+  res.send(err.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`${PORT}번 서버`);
+});

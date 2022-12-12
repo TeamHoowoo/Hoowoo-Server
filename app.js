18cf35485b13a71945217ebadb72086e7c311ea5
ebulsok <leebulsok@gmail.com>
2022-12-12 14:50:23 +0900

    feat: 주문 하기

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 const router = require('./routes/index');
+const userRouter = require('./routes/user');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,6 +17,7 @@ app.use(express.static('public'));
 app.use(cors());
 
 app.use('/', router);
+app.use('/', userRouter);
 
 app.use((err, req, res, next) => {
   console.log(err.stack);
---
+++ routes/user.js
@@ -0,0 +1,38 @@
+// @ts-check
+const express = require('express');
+
+const router = express.Router();
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PW,
+  port: '3306',
+  database: 'hoowoo',
+});
+
+connection.connect();
+
+// 주문 하기
+router.post('/:user_id/order/:store_id/:item_id', (req, res) => {
+  const o = req.body;
+  connection.query(
+    `INSERT INTO order (regDate, count, receipt, letteringColor, creamColor, flavor, size, price, totalPrice, lettering, status, name, phone, address, user_id, store_id, item_id)
+    VALUES (${o.regDate}, ${Number(o.count)},'${o.receipt}', '${
+      o.letteringColor
+    }', '${o.creamColor}', '${o.flavor}', '${o.size}', ${Number(
+      o.price
+    )}, ${Number(o.totalPrice)}, '${o.lettering}', '대기', '${o.name}', '${
+      o.phone
+    }', '${o.address}', ${req.params.user_id}, ${req.params.store_id}, ${
+      req.params.item_id
+    });`,
+    (err) => {
+      if (err) throw err;
+      else res.status(201).json('주문 완료');
+    }
+  );
+});
+
+module.exports = router;

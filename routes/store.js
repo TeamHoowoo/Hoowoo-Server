2d0f4cc3a6304d989a79b2ccccd93d3b7ba3a00a
ebulsok <leebulsok@gmail.com>
2022-12-12 10:55:56 +0900

    feat: 특정 스토어의 모든 제품 정보 반환

--- routes/store.js
+++ routes/store.js
@@ -10,8 +10,20 @@ const connection = mysql.createConnection({
   password: process.env.SQL_PW,
   port: '3306',
   database: 'hoowoo',
+  multipleStatements: true,
 });
 
 connection.connect();
+
+// 특정 스토어의 모든 제품 정보 반환
+router.get('/:store_id/item', (req, res) => {
+  const getItems = `SELECT * FROM item WHERE store_id = '${req.params.store_id}';`;
+  const getStore = `SELECT store_id, storeName, storeAddress, storeIntro, phone, pickupDate FROM store WHERE store_id = '${req.params.store_id}';`;
+
+  connection.query(getItems + getStore, (err, rows) => {
+    if (err) throw err;
+    else res.status(200).json({ items: rows[0], ...rows[1][0] });
+  });
+});
 
 module.exports = router;
